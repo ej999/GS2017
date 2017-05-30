71a447babc0b160980ec7f08522a1f7ad26dd4c6
A Isaac <amroisaac1@gmail.com>
2017-05-30 19:40:29 -0400

    xlsread changes

--- xlsreadtest.m
+++ xlsreadtest.m
@@ -1,15 +1,16 @@
 %csvfile = fopen('test.csv','a+');
 clc
 format longG;
-[dataMatrixNum, dataMatrixText, dataMatrixRaw] = xlsread('28-May-2017 16_54_47.csv');
-[rows, cols] = size(dataMatrixRaw)
+[dataMatrixNum, dataMatrixText, dataMatrixRaw] = xlsread('test.csv');
+[rows, cols] = size(dataMatrixRaw);
 
 dataMatrixGlider = [];
+dataMatrixRaw;
 tic
 for i = 1:1:rows
     if ismember('glider',dataMatrixRaw(i,2))
-        display('Stop sending Container')
-        dataMatrixGlider = [dataMatrixGlider; dataMatrixRaw(i,:)]
+       % display('Stop sending Container');
+        dataMatrixGlider = [dataMatrixGlider; dataMatrixRaw(i,:)];
     else
         dataMatrixRaw(i,2);
     end

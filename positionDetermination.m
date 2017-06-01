3e944b566c25f1e7c5499f0e98461337362f83a7
Jeff1741 <jeffrey.jev@gmail.com>
2017-05-31 22:53:30 -0400

    3D position effects added, incomplete
    Placeholders remain for pitch data and original GPS location at time of deployment

--- positionDetermination.m
+++ positionDetermination.m
@@ -2,18 +2,21 @@ function [ ] = positionDetermination(pk)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 
-    %Take into account 3D height change effects
+    vel = matrix.glider(:,6); % Velocity from pitot tube
+    heading = matrix.glider(:,9); % Direction from magnometer (degrees)
+    pitch = matrix.glider(:,13); % Placeholder for pitch data (degrees)
 
-    %global matrix
-    vel = matrix.glider(:,6)
+    dx(pk) = vel(pk) * cosd(heading(pk)) * cosd(pitch(pk)); % Change in x,y,z position over 1 second update interval
+    dy(pk) = vel(pk) * sind(heading(pk)) * cosd(pitch(pk));
+    dz(pk) = vel(pk) * sind(pitch(pk));
 
-    dir = matrix.glider(:,9);
-    
-    vx = vel * cosd(dir);
-    vy = vel * sind(dir);
-    
+    px = 0; % Original position, to be set to container GPS location at time of deployment
+    py = 0;
 
-    
+    px = cumsum(dx); % Summation of delta changes in x,y,z position
+    py = cumsum(dy);
+    pz = cumsum(dz);
+
     
 end
 
